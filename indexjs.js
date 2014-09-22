aaa7895e4fd1dfd9c295f2139eee88ec13f50df0
David Hanesana <dhanesana@users.noreply.github.com>
2014-09-21 23:54:41 -0700

    add team selection page, jquery animations

--- indexjs.js
+++ indexjs.js
@@ -1,11 +1,9 @@
-var myFunction
 
-function myFunction () {
-	alert ("cool");
-};
+//Everything Fades in
+$('body').hide().fadeIn(400);
 
 //Logo Fade in on Load
-$('#logo', '#start').hide().fadeIn(2500);
+$('#logo').hide().fadeIn(2500);
 
 //Start Button Blinking via StackOverflow
 function blink(selector){
@@ -19,3 +17,87 @@ function blink(selector){
 }
 
 blink("#start");
+
+//Animation on User Click
+ /* $("#logo").click(function(){
+    $("#msg").animate({
+      left:'50%',
+      opacity:'100%',
+    });
+  }); */
+
+// div fading with mouseenter and mouseleave
+$("#header").mouseenter(function(){
+  $("#header").css({
+    "opacity": "0.5"
+  });
+});
+    
+$("#header").mouseleave(function(){
+  $("#header").css({
+    "opacity": "1"
+  });
+});
+
+$("#footer").mouseenter(function(){
+  $("#footer").css({
+    "opacity": "0.5"
+  });
+});
+    
+$("#footer").mouseleave(function(){
+  $("#footer").css({
+    "opacity": "1"
+  });
+});
+
+$("#lettuce").mouseenter(function(){
+  $("#lettuce").css({
+    "opacity": "0.5"
+  });
+});
+    
+$("#lettuce").mouseleave(function(){
+  $("#lettuce").css({
+    "opacity": "1"
+  });
+});
+
+$("#tomato").mouseenter(function(){
+  $("#tomato").css({
+    "opacity": "0.5"
+  });
+});
+    
+$("#tomato").mouseleave(function(){
+  $("#tomato").css({
+    "opacity": "1"
+  });
+});
+
+$("#cheese").mouseenter(function(){
+  $("#cheese").css({
+    "opacity": "0.5"
+  });
+});
+    
+$("#cheese").mouseleave(function(){
+  $("#cheese").css({
+    "opacity": "1"
+  });
+});
+
+$("#start").mouseenter(function(){
+  $("#gamelogo").css({
+    "opacity": "0.5"
+  });
+});
+    
+$("#start").mouseleave(function(){
+  $("#gamelogo").css({
+    "opacity": "1"
+  });
+});
+
+
+
